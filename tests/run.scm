e9b357def542c447a4995c8691fd33802fcb6fcf
Peter Danenberg <pcd@roxygen.org>
2013-09-03 22:31:37 -0700

    Some trivial tests

---
+++ tests/run.scm
@@ -0,0 +1,10 @@
+(use combinatorics
+     test)
+
+(test "Permutations"
+      '((b c) (a c) (c b) (a b) (c a) (b a))
+      (ordered-subset-map values '(a b c) 2))
+
+(test "Combinations"
+      '((c b) (c a) (b a))
+      (unordered-subset-map values '(a b c) 2))
